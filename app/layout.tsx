c90357d4434656b4dfbb8befcf8b30eec8cb154a
gazes12 <khozhainovdmitro@gmail.com>
2024-11-15 21:22:23 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Open_Sans } from 'next/font/google';
 import { cn } from '@/lib/utils';
+import { client } from "@/lib/requestClient";
+import { GetBasicOptionsDocument } from "@/generates/gql/graphql";
 
 const OpenSans = Open_Sans({
   subsets: ['cyrillic'],
@@ -8,6 +10,10 @@ const OpenSans = Open_Sans({
   weight: ['400', '500', '600', '700', '800'],
 });
 
+async function getBasicOptions() {
+  const { basicOptions } = await client.request(GetBasicOptionsDocument);
+  return basicOptions;
+}
 
 export default async function RootLayout({
   children,
