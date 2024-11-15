46c14aebdc0f9b358d92eccb876572af52970663
gazes12 <khozhainovdmitro@gmail.com>
2024-11-15 21:15:17 +0200

    commit

--- app/services/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Service } from '@/components/screens/service';
-import * as React from 'react';
-
-export interface IServicePageProps {
-}
-
-export default function SingleServicePage(props: IServicePageProps) {
-    return <Service />
-}
