4e6a0cbcd5c2b2ba808a2cad187f48cba3883a01
gazes12 <khozhainovdmitro@gmail.com>
2024-11-15 21:34:10 +0200

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,31 @@ import "./globals.css";
 import { Open_Sans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { client } from "@/lib/requestClient";
-import { GetBasicOptionsDocument } from "@/generates/gql/graphql";
-
+import { GetBasicOptionsDocument, GetMetaPageDocument, GetPageOptionsDocument } from "@/generates/gql/graphql";
+import Link from "next/link";
+import Image from "next/image";
+import { headers } from "next/headers";
+import { LayoutComponent } from "@/components/screens/layout";
 const OpenSans = Open_Sans({
   subsets: ['cyrillic'],
   variable: '--font-open-sans',
   weight: ['400', '500', '600', '700', '800'],
 });
 
-async function getBasicOptions() {
+async function getSlug(): Promise<string | null> { // Изменили тип возвращаемого значения
+  const headersList = await headers();
+  const slug = await headersList.get('x-slug');
+  return slug;
+}
+
+async function fetchMain() {
+  const slug = await getSlug();
   const { basicOptions } = await client.request(GetBasicOptionsDocument);
-  return basicOptions;
+  const { optionsPage } = await client.request(GetPageOptionsDocument, { slug });
+  return {
+    basicOptions,
+    optionsPage
+  }
 }
 
 export default async function RootLayout({
@@ -20,13 +34,15 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const data = await fetchMain();
   return (
     <html lang="en">
       <body
         className={cn(OpenSans.className, 'light')}
       >
-        <header>ШАПКА</header>
-        {children}
+        <LayoutComponent data={data} children={children} />
+
       </body>
     </html>
   );
